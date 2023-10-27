82f265645a1d60c73368a0096d1cafa1a04f5db7
jiho <jxxo.ih@gmail.com>
2023-10-27 20:25:04 +0900

    sav

---
+++ src/config.js
@@ -0,0 +1,3 @@
+const USE_LOCATION = window.location.href;
+
+export const API_PATH = USE_LOCATION.indexOf("localhost") < 0 ? "https://port-0-node-learning-12fhqa2llo7zh36o.sel5.cloudtype.app" : "http://localhost:3333";
