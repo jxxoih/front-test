3e9a03e67fea11823a6736c39860fa099141171e
jiho <jxxo.ih@gmail.com>
2023-10-30 14:25:55 +0900

    api 모듈 수정

--- src/App.js
+++ src/App.js
@@ -8,32 +8,35 @@ import * as appConfig from "utills/appUtill";
 
 
 function App() {
-  const [resData, setResData] = useState([]);
+  const [result, setResult] = useState([]);
 
   // https://narup.tistory.com/215  // Promise 참고 사이트
   // Promise 객체를 생성하고 resolve에 parameter를 넣어주면 Promise가 이행상태로 넘어감.
   // 이후 then()을 활용하면 결과 값을 받을 수 있음.
 
   // resolve()를 통해 Promise 실행
-  const getData = () => {
-    return new Promise((resolve, reject) => {
-      const data = appConfig.reqAPI();
-      resolve(data);
-    })
+
+  const clickBtn = () => {
+    const action = document.querySelector("#action").value;
+
+    appConfig.resolveData(action).then((resolvedData) => setResult(resolvedData));
   }
 
   useEffect(() => {
-    getData().then((resolvedData) => setResData(resolvedData));
+    // clickBtn("test");
   }, []);
 
   return (
     <div className="App">
+      <input type="text" id="action" />
+      <button onClick={() => clickBtn()}>버튼클릭</button>
       <p>
         Node DB connected & get data
       </p>
       {
         // DB 통신 결과 데이터
-        resData.map((data) => (
+        !!result &&
+        result.map((data) => (
           <div key={data.s_id}>
             {data.s_name}
           </div>
--- src/utills/appUtill.js
+++ src/utills/appUtill.js
@@ -2,20 +2,27 @@ import axios from 'axios';
 import * as config from "config";
 
 // node 서버 통신 API 모듈
-export const reqAPI = async () => {
-    const exPath = "/test";
+export const reqAPI = async (action) => {
+    const exPath = "/data";
     const reqPath = config.API_PATH + exPath;
     console.log("reqPath", reqPath);
 
     const data = {
-        action: "test"
+        params: {
+            p_id: 1,
+            action: action
+        }
     }
 
-    return await axios.post(reqPath, null, {
-        params: {
-            action: "test"
-        }
-    })
+    return await axios.post(reqPath, null, data)
         .then((res) => res.data.data)
         .catch((err) => { console.log("err", err) })
+}
+
+export const resolveData = (action) => {
+    return new Promise((resolve, reject) => {
+        const data = reqAPI(action);
+        resolve(data);
+        reject([])
+    })
 }
